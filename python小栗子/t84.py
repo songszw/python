5fff5939df346f463585d3b4fe80f77e90341a4a
songszw <songszw@users.noreply.github.com>
2017-02-15 17:09:04 +0800

    add
    add

---
+++ python小栗子/t84.py
@@ -0,0 +1,15 @@
+import os
+
+all_files = os.listdir(os.curdir)
+type_dict = dict()
+
+for each in all_files:
+    if os.path.isdir(each):
+        type_dict.setdefault('文件夹',0)
+        type_dict['文件夹']+=1
+    else:
+        ext = os.path.splitext(each)[1]
+        type_dict.setdefault(ext,0)
+        type_dict[ext]+=1
+for each_type in type_dict.keys():
+    print('该文件夹下共有类型为【%s】的文件%d个'% (each_type,type_dict[each_type]))
