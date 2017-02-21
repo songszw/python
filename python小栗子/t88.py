a9104ec6de5449ae2644f661500a61f8fa9908ae
songszw <songszw@users.noreply.github.com>
2017-02-21 11:18:13 +0800

    add
    add

---
+++ python小栗子/t88.py
@@ -0,0 +1,8 @@
+"""---The First Game---"""
+temp = input('Guess what number i want to say:')
+guess = int(temp)
+if guess==8:
+    print('Wow, that`s great')
+    print('Bingo')
+else:
+    print('wrong number')
