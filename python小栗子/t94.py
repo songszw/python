f5862fbd13c03c70a3666125a439453628343be4
songszw <songszw@users.noreply.github.com>
2017-03-03 16:55:43 +0800

    add
    add

---
+++ python小栗子/t94.py
@@ -0,0 +1,13 @@
+num = int(input('please type a number:'))
+if 100>=num>=90:
+    print('a')
+elif 90>num>=80:
+    print('b')
+elif 80>num>=70:
+    print('c')
+elif 70>num>=60:
+    print('d')
+elif 60>num>=0:
+    print('great')
+else:
+    print('wrong number')
